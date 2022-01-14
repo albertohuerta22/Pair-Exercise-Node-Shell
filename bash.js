f6b0b3fa24691274111ebf8f5911bf37f18f8bed
Rob Watkins <rwatkins123@gmail.com>
2022-01-14 14:21:04 -0600

    cwd

---
+++ bash.js
@@ -0,0 +1,16 @@
+
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) =>{
+  const cmd = data.toString().trim();
+
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\npromp > ');
+});
+
+import { cwd } from 'process';
+
+console.log(`Current directory: ${cwd()}`);
+
+// console.log();
